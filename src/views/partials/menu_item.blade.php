d22c8232784769c1742269d26277124b10b705ed
h.narozny <h.narozny@merixstudio.com>
2018-06-18 13:11:30 +0200

    Use relative path for custom routes

--- src/views/partials/menu_item.blade.php
+++ src/views/partials/menu_item.blade.php
@@ -20,7 +20,7 @@
 		@elseif (strpos($key, $pagePrefix) === 0)
 			<a href="{{route('admin_page', array(substr($key, strlen($pagePrefix)), false))}}">{{$item}}</a>
 		@elseif (strpos($key, $routePrefix) === 0)
-			<a href="{{route(substr($key, strlen($routePrefix)))}}">{{$item}}</a>
+			<a href="{{route(substr($key, strlen($routePrefix)), [], false)}}">{{$item}}</a>
 		@else
 			<a href="{{route('admin_index', array($key), false)}}">{{$item}}</a>
 		@endif
