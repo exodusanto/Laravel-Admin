9e0cebad72fadc186118db2373cef1e7e5f7be71
Antonio Dal Sie <antonio.dalsie@akqa.com>
2019-07-10 00:03:43 +0200

    updated: 5.8

--- src/Frozennode/Administrator/AdministratorServiceProvider.php
+++ src/Frozennode/Administrator/AdministratorServiceProvider.php
@@ -9,140 +9,131 @@ use Frozennode\Administrator\Config\Factory as ConfigFactory;
 use Frozennode\Administrator\Actions\Factory as ActionFactory;
 use Frozennode\Administrator\DataTable\Columns\Factory as ColumnFactory;
 
-class AdministratorServiceProvider extends ServiceProvider {
+class AdministratorServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->loadViewsFrom(__DIR__.'/../../views', 'administrator');
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/../../views', 'administrator');
 
-		$this->mergeConfigFrom(
-			__DIR__.'/../../config/administrator.php', 'administrator'
-		);
+        $this->mergeConfigFrom(
+            __DIR__.'/../../config/administrator.php',
+            'administrator'
+        );
 
-		$this->loadTranslationsFrom(__DIR__.'/../../lang', 'administrator');
+        $this->loadTranslationsFrom(__DIR__.'/../../lang', 'administrator');
 
-		$this->publishes([
-			__DIR__.'/../../config/administrator.php' => config_path('administrator.php'),
-		]);
+        $this->publishes([
+            __DIR__.'/../../config/administrator.php' => config_path('administrator.php'),
+        ]);
 
-		$this->publishes([
-			__DIR__.'/../../../public' => public_path('packages/frozennode/administrator'),
-		], 'public');
+        $this->publishes([
+            __DIR__.'/../../../public' => public_path('packages/frozennode/administrator'),
+        ], 'public');
 
-		//set the locale
-		$this->setLocale();
+        //set the locale
+        $this->setLocale();
 
-		$this->app['events']->fire('administrator.ready');
-	}
+        $this->app['events']->dispatch('administrator.ready');
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//include our view composers, and routes to avoid issues with catch-all routes defined by users
-		include __DIR__.'/../../viewComposers.php';
-		include __DIR__.'/../../routes.php';
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //include our view composers, and routes to avoid issues with catch-all routes defined by users
+        include __DIR__.'/../../viewComposers.php';
+        include __DIR__.'/../../routes.php';
 
-		//the admin validator
-		$this->app->singleton('admin_validator', function($app)
-		{
-			//get the original validator class so we can set it back after creating our own
-			$originalValidator = LValidator::make(array(), array());
-			$originalValidatorClass = \get_class($originalValidator);
+        //the admin validator
+        $this->app->singleton('admin_validator', function ($app) {
+            //get the original validator class so we can set it back after creating our own
+            $originalValidator = LValidator::make(array(), array());
+            $originalValidatorClass = \get_class($originalValidator);
 
-			//temporarily override the core resolver
-			LValidator::resolver(function($translator, $data, $rules, $messages, $customAttributes) use ($app)
-			{
-				$validator = new Validator($translator, $data, $rules, $messages, $customAttributes);
-				$validator->setUrlInstance($app->make('url'));
-				return $validator;
-			});
+            //temporarily override the core resolver
+            LValidator::resolver(function ($translator, $data, $rules, $messages, $customAttributes) use ($app) {
+                $validator = new Validator($translator, $data, $rules, $messages, $customAttributes);
+                $validator->setUrlInstance($app->make('url'));
+                return $validator;
+            });
 
-			//grab our validator instance
-			$validator = LValidator::make(array(), array());
+            //grab our validator instance
+            $validator = LValidator::make(array(), array());
 
-			//set the validator resolver back to the original validator
-			LValidator::resolver(function($translator, $data, $rules, $messages, $customAttributes) use ($originalValidatorClass)
-			{
-				return new $originalValidatorClass($translator, $data, $rules, $messages, $customAttributes);
-			});
+            //set the validator resolver back to the original validator
+            LValidator::resolver(function ($translator, $data, $rules, $messages, $customAttributes) use ($originalValidatorClass) {
+                return new $originalValidatorClass($translator, $data, $rules, $messages, $customAttributes);
+            });
 
-			//return our validator instance
-			return $validator;
-		});
+            //return our validator instance
+            return $validator;
+        });
 
-		//set up the singleton instances
-		$this->app->singleton('admin_config_factory', function($app)
-		{
-			return new ConfigFactory($app->make('admin_validator'), LValidator::make(array(), array()), config('administrator'));
-		});
+        //set up the singleton instances
+        $this->app->singleton('admin_config_factory', function ($app) {
+            return new ConfigFactory($app->make('admin_validator'), LValidator::make(array(), array()), config('administrator'));
+        });
 
-		$this->app->singleton('admin_field_factory', function($app)
-		{
-			return new FieldFactory($app->make('admin_validator'), $app->make('itemconfig'), $app->make('db'));
-		});
+        $this->app->singleton('admin_field_factory', function ($app) {
+            return new FieldFactory($app->make('admin_validator'), $app->make('itemconfig'), $app->make('db'));
+        });
 
-		$this->app->singleton('admin_datatable', function($app)
-		{
-			$dataTable = new DataTable($app->make('itemconfig'), $app->make('admin_column_factory'), $app->make('admin_field_factory'));
-			$dataTable->setRowsPerPage($app->make('session.store'), config('administrator.global_rows_per_page'));
+        $this->app->singleton('admin_datatable', function ($app) {
+            $dataTable = new DataTable($app->make('itemconfig'), $app->make('admin_column_factory'), $app->make('admin_field_factory'));
+            $dataTable->setRowsPerPage($app->make('session.store'), config('administrator.global_rows_per_page'));
 
-			return $dataTable;
-		});
+            return $dataTable;
+        });
 
-		$this->app->singleton('admin_column_factory', function($app)
-		{
-			return new ColumnFactory($app->make('admin_validator'), $app->make('itemconfig'), $app->make('db'));
-		});
+        $this->app->singleton('admin_column_factory', function ($app) {
+            return new ColumnFactory($app->make('admin_validator'), $app->make('itemconfig'), $app->make('db'));
+        });
 
-		$this->app->singleton('admin_action_factory', function($app)
-		{
-			return new ActionFactory($app->make('admin_validator'), $app->make('itemconfig'), $app->make('db'));
-		});
+        $this->app->singleton('admin_action_factory', function ($app) {
+            return new ActionFactory($app->make('admin_validator'), $app->make('itemconfig'), $app->make('db'));
+        });
 
-		$this->app->singleton('admin_menu', function($app)
-		{
-			return new Menu($app->make('config'), $app->make('admin_config_factory'));
-		});
-	}
+        $this->app->singleton('admin_menu', function ($app) {
+            return new Menu($app->make('config'), $app->make('admin_config_factory'));
+        });
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('admin_validator', 'admin_config_factory', 'admin_field_factory', 'admin_datatable', 'admin_column_factory',
-			'admin_action_factory', 'admin_menu');
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array('admin_validator', 'admin_config_factory', 'admin_field_factory', 'admin_datatable', 'admin_column_factory',
+            'admin_action_factory', 'admin_menu');
+    }
 
-	/**
-	 * Sets the locale if it exists in the session and also exists in the locales option
-	 *
-	 * @return void
-	 */
-	public function setLocale()
-	{
-		if ($locale = $this->app->session->get('administrator_locale'))
-		{
-			$this->app->setLocale($locale);
-		}
-	}
-
+    /**
+     * Sets the locale if it exists in the session and also exists in the locales option
+     *
+     * @return void
+     */
+    public function setLocale()
+    {
+        if ($locale = $this->app->session->get('administrator_locale')) {
+            $this->app->setLocale($locale);
+        }
+    }
 }
