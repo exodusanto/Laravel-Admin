4f00c2692dfcfec3af4ae905d854d8ddad20e24d
Antonio Dal Sie <antonydalsie@gmail.com>
2020-06-30 11:58:32 +0200

    Update validateArray definition

--- src/Frozennode/Administrator/Validator.php
+++ src/Frozennode/Administrator/Validator.php
@@ -5,63 +5,63 @@ use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Query\Builder;
 
-class Validator extends \Illuminate\Validation\Validator {
+class Validator extends \Illuminate\Validation\Validator
+{
+    protected $overrideCustomMessages = array(
+        'string' => "The :attribute option must be a string",
+        'directory' => "The :attribute option must be a valid directory",
+        'array' => "The :attribute option must be an array",
+        'array_with' => "The :attribute array is missing some required values",
+        'not_empty' => "The :attribute option must not be empty",
+        'callable' => "The :attribute option must be a function",
+        'eloquent' => "The :attribute option must be the string name of a valid Eloquent model",
+    );
 
-	protected $overrideCustomMessages = array(
-		'string' => "The :attribute option must be a string",
-		'directory' => "The :attribute option must be a valid directory",
-		'array' => "The :attribute option must be an array",
-		'array_with' => "The :attribute array is missing some required values",
-		'not_empty' => "The :attribute option must not be empty",
-		'callable' => "The :attribute option must be a function",
-		'eloquent' => "The :attribute option must be the string name of a valid Eloquent model",
-	);
+    /**
+     * The URL instance
+     *
+     * @var \Illuminate\Routing\UrlGenerator
+     */
+    protected $url;
 
-	/**
-	 * The URL instance
-	 *
-	 * @var \Illuminate\Routing\UrlGenerator
-	 */
-	protected $url;
+    /**
+     * Injects the URL class instance
+     *
+     * @param \Illuminate\Routing\UrlGenerator $url
+     *
+     * @return void
+     */
+    public function setUrlInstance(\Illuminate\Routing\UrlGenerator $url)
+    {
+        $this->url = $url;
+    }
 
-	/**
-	 * Injects the URL class instance
-	 *
-	 * @param \Illuminate\Routing\UrlGenerator $url
-	 *
-	 * @return void
-	 */
-	public function setUrlInstance(\Illuminate\Routing\UrlGenerator $url)
-	{
-		$this->url = $url;
-	}
+    /**
+     * Gets the URL class instance
+     *
+     * @return \Illuminate\Routing\UrlGenerator
+     */
+    public function getUrlInstance()
+    {
+        return $this->url;
+    }
 
-	/**
-	 * Gets the URL class instance
-	 *
-	 * @return \Illuminate\Routing\UrlGenerator
-	 */
-	public function getUrlInstance()
-	{
-		return $this->url;
-	}
+    /**
+     * Overrides the rules and data
+     *
+     * @param array		$data
+     * @param array		$rules
+     *
+     * @return void
+     */
+    public function override($data, $rules)
+    {
+        $this->setData($data);
+        $this->setRules($rules);
+        $this->setCustomMessages($this->overrideCustomMessages);
+    }
 
-	/**
-	 * Overrides the rules and data
-	 *
-	 * @param array		$data
-	 * @param array		$rules
-	 *
-	 * @return void
-	 */
-	public function override($data, $rules)
-	{
-		$this->setData($data);
-		$this->setRules($rules);
-		$this->setCustomMessages($this->overrideCustomMessages);
-	}
-
-	protected function explodeRules(array $rules)
+    protected function explodeRules(array $rules)
     {
         foreach ($rules as $key => $rule) {
             if (Str::contains($key, '*')) {
@@ -80,144 +80,140 @@ class Validator extends \Illuminate\Validation\Validator {
         return $rules;
     }
 
-	/**
-	 * Sets the rules
-	 *
-	 * @param array		$rules
-	 */
-	public function setRules(array $rules)
-	{
-		$this->rules = $this->explodeRules($rules);
-	}
+    /**
+     * Sets the rules
+     *
+     * @param array		$rules
+     */
+    public function setRules(array $rules)
+    {
+        $this->rules = $this->explodeRules($rules);
+    }
 
-	/**
-	 * Mimic of the Laravel array_get helper
-	 *
-	 * @param  array   $array
-	 * @param  string  $key
-	 * @param  mixed   $default
-	 *
-	 * @return mixed
-	 */
-	public function arrayGet($array, $key, $default = null)
-	{
-		if (\is_null($key)) return $array;
+    /**
+     * Mimic of the Laravel array_get helper
+     *
+     * @param  array   $array
+     * @param  string  $key
+     * @param  mixed   $default
+     *
+     * @return mixed
+     */
+    public function arrayGet($array, $key, $default = null)
+    {
+        if (\is_null($key)) {
+            return $array;
+        }
 
-		if (isset($array[$key])) return $array[$key];
+        if (isset($array[$key])) {
+            return $array[$key];
+        }
 
-		foreach (explode('.', $key) as $segment)
-		{
-			if ( ! \is_array($array) or ! array_key_exists($segment, $array))
-			{
-				return value($default);
-			}
+        foreach (explode('.', $key) as $segment) {
+            if (! \is_array($array) or ! array_key_exists($segment, $array)) {
+                return value($default);
+            }
 
-			$array = $array[$segment];
-		}
+            $array = $array[$segment];
+        }
 
-		return $array;
-	}
+        return $array;
+    }
 
-	/**
-	 * Checks if a table is already joined to a query object
-	 *
-	 * @param Query		$query
-	 * @param string	$table
-	 *
-	 * @return bool
-	 */
-	public function isJoined($query, $table)
-	{
-		$tableFound = false;
-		$query = is_a($query, Builder::class) ? $query : $query->getQuery();
+    /**
+     * Checks if a table is already joined to a query object
+     *
+     * @param Query		$query
+     * @param string	$table
+     *
+     * @return bool
+     */
+    public function isJoined($query, $table)
+    {
+        $tableFound = false;
+        $query = is_a($query, Builder::class) ? $query : $query->getQuery();
 
-		if ($query->joins)
-		{
-			//iterate over the joins to see if the table is there
-			foreach ($query->joins as $join)
-			{
-				if ($join->table === $table)
-				{
-					return true;
-				}
-			}
-		}
+        if ($query->joins) {
+            //iterate over the joins to see if the table is there
+            foreach ($query->joins as $join) {
+                if ($join->table === $table) {
+                    return true;
+                }
+            }
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Validates that an item is a directory
-	 */
-	public function validateDirectory($attribute, $value, $parameters)
-	{
-		return is_dir($value);
-	}
+    /**
+     * Validates that an item is a directory
+     */
+    public function validateDirectory($attribute, $value, $parameters)
+    {
+        return is_dir($value);
+    }
 
-	/**
-	 * Validates that an item is an array
-	 */
-	public function validateArray($attribute, $value)
-	{
-		return \is_array($value);
-	}
+    /**
+     * Validates that an item is an array
+     */
+    public function validateArray($attribute, $value, $parameters = [])
+    {
+        return \is_array($value);
+    }
 
-	/**
-	 * Validates that an item is an array
-	 */
-	public function validateArrayWithAllOrNone($attribute, $value, $parameters)
-	{
-		$missing = 0;
+    /**
+     * Validates that an item is an array
+     */
+    public function validateArrayWithAllOrNone($attribute, $value, $parameters)
+    {
+        $missing = 0;
 
-		foreach ($parameters as $key)
-		{
-			if (!isset($value[$key]))
-			{
-				$missing++;
-			}
-		}
+        foreach ($parameters as $key) {
+            if (!isset($value[$key])) {
+                $missing++;
+            }
+        }
 
-		return $missing === Util::count($parameters) || $missing === 0;
-	}
+        return $missing === Util::count($parameters) || $missing === 0;
+    }
 
-	/**
-	 * Validates that an item is not empty
-	 */
-	public function validateNotEmpty($attribute, $value, $parameters)
-	{
-		return !empty($value);
-	}
+    /**
+     * Validates that an item is not empty
+     */
+    public function validateNotEmpty($attribute, $value, $parameters)
+    {
+        return !empty($value);
+    }
 
-	/**
-	 * Validates that an item is callable
-	 */
-	public function validateCallable($attribute, $value, $parameters)
-	{
-		return \is_callable($value);
-	}
+    /**
+     * Validates that an item is callable
+     */
+    public function validateCallable($attribute, $value, $parameters)
+    {
+        return \is_callable($value);
+    }
 
-	/**
-	 * Validate that an attribute is a string.
-	 */
-	public function validateString($attribute, $value)
-	{
-		return \is_string($value);
-	}
+    /**
+     * Validate that an attribute is a string.
+     */
+    public function validateString($attribute, $value)
+    {
+        return \is_string($value);
+    }
 
-	/**
-	 * Validates that an item is either a string or callable
-	 */
-	public function validateStringOrCallable($attribute, $value, $parameters)
-	{
-		return \is_string($value) || \is_callable($value);
-	}
+    /**
+     * Validates that an item is either a string or callable
+     */
+    public function validateStringOrCallable($attribute, $value, $parameters)
+    {
+        return \is_string($value) || \is_callable($value);
+    }
 
-	/**
-	 * Validates that an item is an Eloquent model
-	 */
-	public function validateEloquent($attribute, $value, $parameters)
-	{
-		return class_exists($value) && is_a(new $value, Model::class);
-	}
-
+    /**
+     * Validates that an item is an Eloquent model
+     */
+    public function validateEloquent($attribute, $value, $parameters)
+    {
+        return class_exists($value) && is_a(new $value, Model::class);
+    }
 }
